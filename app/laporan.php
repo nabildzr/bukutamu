1e9788ea285a64b8a4604ec2b40a72fb2babf1d0
nabildzr <muhammadnbillz@gmail.com>
2024-09-21 20:31:05 +0700

    Updated Laporan

--- app/laporan.php
+++ app/laporan.php
@@ -6,7 +6,7 @@ require_once('../conf/koneksi.php');
 <!-- Begin Page Content -->
 <div class="container-fluid">
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Laporan</h1>
+    <h1 class="h3 mb-4  text-gray-800">Laporan</h1>
 
     <!-- Form untuk memilih periode tanggal -->
     <form method="GET" action="">
@@ -22,52 +22,111 @@ require_once('../conf/koneksi.php');
                 <input type="date" id="end_date" name="end_date" class="form-control" required>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Tampilkan Laporan</button>
+        <button type="submit" name="show" class="btn btn-primary">Tampilkan Laporan</button>
     </form>
 
+
+    <div class="card shadow mb-4">
+      
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Tanggal</th>
+                            <th>Nama Tamu</th>
+                            <th>Alamat</th>
+                            <th>No. Telp/HP</th>
+                            <th>Bertemu Dengan</th>
+                            <th>Kepentingan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        if (isset($_POST['show'])) {
+                            $start_date = $_POST['start_date'];
+                            $end_date = $_POST['end_date'];
+
+                            $no = 1;
+
+                            $buku_tamu = query("SELECT * FROM tb_bukutamu WHERE tanggal BETWEEN '$start_date' AND '$end_date'");
+                            foreach ($buku_tamu as $tamu): ?>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td><?= $tamu['tanggal'] ?></td>
+                                    <td><?= $tamu['nama_tamu'] ?></td>
+                                    <td><?= $tamu['alamat'] ?></td>
+                                    <td><?= $tamu['no_hp'] ?></td>
+                                    <td><?= $tamu['bertemu'] ?></td>
+                                    <td><?= $tamu['kepentingan'] ?></td>
+                                    <td>
+                                        <a href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>" class="btn btn-success">Ubah</a>
+                                        <a href="javascript:void(0);" onclick="confirmDeletion('<?= $tamu['id_tamu'] ?>')" class="btn btn-danger">Hapus</a>
+                                        <script>
+                                            function confirmDeletion(id) {
+                                                Swal.fire({
+                                                    title: 'Apakah anda yakin?',
+                                                    text: "Anda tidak akan bisa mengembalikan data ini!",
+                                                    icon: 'warning',
+                                                    showCancelButton: true,
+                                                    confirmButtonColor: '#3085d6',
+                                                    cancelButtonColor: '#d33',
+                                                    confirmButtonText: 'Ya, hapus!',
+                                                    cancelButtonText: 'Batal'
+                                                }).then((result) => {
+                                                    if (result.isConfirmed) {
+                                                        window.location.href = 'hapus-tamu.php?id=' + id;
+                                                    }
+                                                })
+                                            }
+                                        </script>
+                                    </td>
+                                </tr>
+                        <?php endforeach;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- old code -->
     <?php
-    if (isset($_GET['start_date']) && isset($_GET['end_date'])) {
-        $start_date = $_GET['start_date'];
-        $end_date = $_GET['end_date'];
+    // if (isset($_GET['start_date']) && isset($_GET['end_date'])) {
+    //     $start_date = $_GET['start_date'];
+    //     $end_date = $_GET['end_date'];
 
-        // Query untuk mengambil data berdasarkan periode tanggal
-        $query = "SELECT * FROM tb_bukutamu WHERE tanggal BETWEEN '$start_date' AND '$end_date'";
-        $result = mysqli_query($conn, $query);
+    //     // Query untuk mengambil data berdasarkan periode tanggal
+    //     $query = "SELECT * FROM tb_bukutamu WHERE tanggal BETWEEN '$start_date' AND '$end_date'";
+    //     $result = mysqli_query($conn, $query);
 
-        if (mysqli_num_rows($result) > 0) {
-            echo '<table class="table table-bordered mt-4">';
-            echo '<thead>';
-            echo '<tr>';
-            echo '<th>No</th>';
-            echo '<th>Tanggal</th>';
-            echo '<th>Nama Tamu</th>';
-            echo '<th>Alamat</th>';
-            echo '<th>No. Telp/HP</th>';
-            echo '<th>Bertemu Dengan</th>';
-            echo '<th>Kepentingan</th>';
-            echo '</tr>';
-            echo '</thead>';
-            echo '<tbody>';
+    //     if (mysqli_num_rows($result) > 0) {
+    //         echo '
+    //         ';
 
-            $no = 1;
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr>';
-                echo '<td>' . $no++ . '</td>';
-                echo '<td>' . $row['tanggal'] . '</td>';
-                echo '<td>' . $row['nama_tamu'] . '</td>';
-                echo '<td>' . $row['alamat'] . '</td>';
-                echo '<td>' . $row['no_hp'] . '</td>';
-                echo '<td>' . $row['bertemu'] . '</td>';
-                echo '<td>' . $row['kepentingan'] . '</td>';
-                echo '</tr>';
-            }
+    //         $no = 1;
+    //         while ($row = mysqli_fetch_assoc($result)) {
+    //             echo '<tr>';
+    //             echo '<td>' . $no++ . '</td>';
+    //             echo '<td>' . $row['tanggal'] . '</td>';
+    //             echo '<td>' . $row['nama_tamu'] . '</td>';
+    //             echo '<td>' . $row['alamat'] . '</td>';
+    //             echo '<td>' . $row['no_hp'] . '</td>';
+    //             echo '<td>' . $row['bertemu'] . '</td>';
+    //             echo '<td>' . $row['kepentingan'] . '</td>';
+    //             echo '</tr>';
+    //         }
 
-            echo '</tbody>';
-            echo '</table>';
-        } else {
-            echo '<p class="mt-4">Tidak ada data untuk periode yang dipilih.</p>';
-        }
-    }
+    //         echo '</tbody>';
+    //         echo '</table>';
+    //     } else {
+    //         echo '<p class="mt-4">Tidak ada data untuk periode yang dipilih.</p>';
+    //     }
+    // }
     ?>
 
     <!-- Button to export data to Excel -->
